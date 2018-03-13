ab5c3f8a421436be1f7597b23ccb507b4414f999
logonrm <logonrm@fiap.com.br>
2018-03-12 22:34:00 -0300

    first commit

---
+++ Assets/scripts/PersonagemScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemScript : MonoBehaviour {
+
+
+	public float velocidade;
+	public GameObject personagem;
+	Animation anim;
+	Vector3 novaPosicao;
+
+
+
+	void Start () {
+
+		// captura a posicao o personagem
+		novaPosicao = transform.position;
+		anim = personagem.GetComponent<Animation> ();
+	
+		//Define a animação inicial o personagem
+		anim.CrossFade("idle");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// touch ou clique do mouse
+		if (Input.GetButton ("Fire1")) {
+
+			// transfoma o toque na tela em uma coordenada 3d
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+			//obtem a nova posicao
+
+			if (Physics.Raycast (ray, out hit)) {
+				novaPosicao = hit.point;
+			}
+
+			// move o personagem
+			transform.position = Vector3.MoveTowards (transform.position,
+				novaPosicao, velocidade * Time.deltaTime);
+			transform.LookAt (hit.point);
+
+			anim.CrossFade ("run");
+		} else {
+			//animacao e idle
+			anim.CrossFade ("idle");
+		}
+}
+}
---
+++ Assets/scripts/gerarAboboras.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gerarAboboras : MonoBehaviour {
+
+	public float limiteEsquerdo, limiteDireito, limiteFrontal, limiteTraseiro;
+	public float frequencia;
+	public GameObject aboboaPrefab;
+
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		yield return new WaitForSeconds(Random.Range (0.5f, frequencia));
+		Vector3 posicao;
+		posicao.x = Random.Range (limiteEsquerdo, limiteDireito);
+		posicao.y = transform.position.y;
+		posicao.z = Random.Range (limiteFrontal, limiteTraseiro);
+		Instantiate (aboboaPrefab, posicao, transform.rotation);
+		StartCoroutine (Start ());
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
